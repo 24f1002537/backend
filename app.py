ce307027dd1bf4ad195b802b2d0389ab1a27c068
24f1002537 <24f1002537@ds.study.iitm.ac.in>
2025-06-17 07:11:08 +0530

    second try

--- app.py
+++ app.py
@@ -97,4 +97,5 @@ def index():
     return render_template("index.html", answer=answer_text)
 
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
